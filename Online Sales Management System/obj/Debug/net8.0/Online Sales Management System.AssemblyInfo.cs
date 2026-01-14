9feb8ff3a2590769387c44215739a51b282bc26c
Alucard30Dec <alucard30dec@gmail.com>
2026-01-14 14:23:49 +0700

    Update

--- Online Sales Management System/obj/Debug/net8.0/Online Sales Management System.AssemblyInfo.cs
+++ Online Sales Management System/obj/Debug/net8.0/Online Sales Management System.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Online Sales Management System")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b64266ebfa8581f6e9a14afdc38e6ec2e04caf76")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6f862774b2f8dac9bc7e987e546f77d52e37085")]
 [assembly: System.Reflection.AssemblyProductAttribute("Online Sales Management System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Online Sales Management System")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
