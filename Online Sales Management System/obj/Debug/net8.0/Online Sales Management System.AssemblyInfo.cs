e2f2bd9f4632baa2554a14f12024900acf18e205
Alucard30Dec <alucard30dec@gmail.com>
2026-03-27 16:46:21 +0700

    Configure run with ctrl shift b

--- Online Sales Management System/obj/Debug/net8.0/Online Sales Management System.AssemblyInfo.cs
+++ Online Sales Management System/obj/Debug/net8.0/Online Sales Management System.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Online Sales Management System")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a97b80cf37f7e070ed865f0c5639b1d5beb5d512")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7279476038b373251cf5102c1a7fd997732d7186")]
 [assembly: System.Reflection.AssemblyProductAttribute("Online Sales Management System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Online Sales Management System")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
